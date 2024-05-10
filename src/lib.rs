cf98bd5b80ef32af8f2e1d2217c26e05a5b71746
John Wass <wassj@ctc.com>
2024-05-10 00:47:01 -0400

    unique

--- src/lib.rs
+++ src/lib.rs
@@ -102,7 +102,7 @@ pub fn a(df: PyLazyFrame) -> PyDataFrame {
     let df = df.group_by(["mmsi"])
         .agg([
             len(),
-            col("t").sort(SortOptions::default()),
+            col("t").sort(SortOptions::default()).unique(),
             concat_str([col("lon"), col("lat")], " ", true).alias("p"),
         ])
         .collect().expect("lazy");
