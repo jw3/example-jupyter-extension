45971276713cc6d8ec50416732445e991120c0a1
John Wass <wassj@ctc.com>
2024-06-06 15:08:38 -0400

    support for line strings

--- python/keplerviz/magics.py
+++ python/keplerviz/magics.py
@@ -1,7 +1,7 @@
 from IPython.core.magic import Magics, magics_class, line_magic
 from IPython import get_ipython
 
-from keplerviz.keplerviz import load_ais_csv
+from keplerviz.keplerviz import animate_ais_csv, render_ais_csv
 
 
 @magics_class
@@ -9,8 +9,11 @@ class CustomMagics(Magics):
     @line_magic
     def ais(self, line):
         splits = line.split(' ')
-        if splits[0] == 'load':
-            r = load_ais_csv(splits[1])
+        if splits[0] == 'animate':
+            r = animate_ais_csv(splits[1])
+            return r
+        elif splits[0] == 'render':
+            r = render_ais_csv(splits[1])
             return r
         else:
             return f"Unknown command {line}"
